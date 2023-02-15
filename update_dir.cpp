3e9b08880376f3fbaef27c101c0ee2a0be7d683e
Dominik Wójt <domin144@o2.pl>
2023-02-15 20:06:36 +0100

    move options parsing to separate function

--- update_dir.cpp
+++ update_dir.cpp
@@ -1,10 +1,22 @@
 #include <boost/program_options.hpp>
+#include <exception>
 #include <filesystem>
 #include <iostream>
 
+struct just_exit : std::exception
+{
+};
+
 namespace po = boost::program_options;
 
-int main(const int argc, const char* const argv[])
+struct options
+{
+	std::string source_directory;
+	std::string target_directory;
+	bool dry_run;
+};
+
+options parse_options(const int argc, const char* const argv[])
 {
 	po::options_description description("Allowed options");
 	description.add_options() //
@@ -21,29 +33,52 @@ int main(const int argc, const char* const argv[])
 	po::store(
 		po::command_line_parser(argc, argv)
 			.options(description)
-			.positional(positional_description).run(),
+			.positional(positional_description)
+			.run(),
 		vm);
 	po::notify(vm);
 
 	if (vm.count("help"))
 	{
 		std::cout << description << std::endl;
-		return 1;
+		throw just_exit {};
 	}
 
 	const bool valid_command_line =
 		vm.count("source-dir") == 1 && vm.count("target-dir") == 1;
 	if (!valid_command_line)
 	{
-		std::cerr << "invalid parameters" << std::endl;
 		std::cout << description << std::endl;
-		return 1;
+		throw std::runtime_error{"invalid arguments"};
 	}
 
-	std::string source = vm["source-dir"].as<std::string>();
-	std::string target = vm["target-dir"].as<std::string>();
+	options result;
+	result.source_directory = vm["source-dir"].as<std::string>();
+	result.target_directory = vm["target-dir"].as<std::string>();
+	result.dry_run = vm.count("dry-run");
+	return result;
+}
 
-	std::cout << "from " << source << " to " << target << std::endl;
+int main(const int argc, const char* const argv[])
+try
+{
+	const options options_0 = parse_options(argc, argv);
+
+	std::cout << "from " << options_0.source_directory << " to "
+			  << options_0.target_directory << '\n';
+	if (options_0.dry_run)
+	{
+		std::cout << "dry run" << '\n';
+	}
 
 	return 0;
+}
+catch (just_exit&)
+{
+	return 1;
+}
+catch (std::exception& e)
+{
+	std::cerr << "standard exception caught: " << e.what() << std::endl;
+	return 1;
 }
